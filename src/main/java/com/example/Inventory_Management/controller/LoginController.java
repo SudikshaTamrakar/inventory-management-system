c2457f5c36738a22bdbbe4bd1ec7236c49a6ddf1
unknown <sudiksha085@gmail.com>
2025-11-24 21:00:33 +0545

    Login Controller

--- src/main/java/com/example/Inventory_Management/controller/LoginController.java
+++ src/main/java/com/example/Inventory_Management/controller/LoginController.java
@@ -1,4 +1,12 @@
 package com.example.Inventory_Management.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
 public class LoginController {
+    @Autowired
+
 }
